12e40a47185aa10767a45d7cebe8cdc1e734b9f9
saisrinivaspaila <pailasaisrinivas7668@gmail.com>
2020-04-24 03:16:54 +0530

    Done without Media queries

--- lib/aggregate.dart
+++ lib/aggregate.dart
@@ -66,12 +66,15 @@ class _AggregateDetailsState extends State<AggregateDetails> {
                     ),
                   ),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SemesterData(index),
-                      ),
-                    );
+                    if (semestergpa[index][0] != "Backlogs" &&
+                        semestergpa[index][0] != "Total GPA") {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SemesterData(index),
+                        ),
+                      );
+                    }
                   },
                 ),
               );
@@ -124,7 +127,7 @@ class _AggregateDetailsState extends State<AggregateDetails> {
       semestergpa.add(["Semester " + key, value]);
       val += value;
     });
-    semestergpa.add(["Backlog Count", count]);
+    semestergpa.add(["Backlogs", count]);
     if (count == 0) {
       semestergpa.add([
         "Total GPA",
@@ -160,17 +163,98 @@ class _SemesterDataState extends State<SemesterData> {
     });
 
     return Scaffold(
-      body: Container(
-        child: ListView.builder(
-          itemCount: subjAndGrade.length,
-          itemBuilder: (context, index) {
-            return ListBody(
-              children: [
-                Text('${subjAndGrade[index][0]} - ${subjAndGrade[index][1]}')
-              ],
-            );
-          },
-        ),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 100.0,
+          ), //add media query for this on
+          SizedBox(
+            height: 100.0,
+            child: Image.asset(
+              "images/Vignan_logo.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+          SizedBox(
+            height: 90.0,
+          ),
+
+          Text(
+            '${semestergpa[index][0]} results',
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+
+          SizedBox(height: 20.0),
+
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: subjAndGrade.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 10),
+                child: RaisedButton(
+                  color: Color(0xff3949ab),
+                  hoverElevation: 3.0,
+                  elevation: 1.0,
+                  // padding: EdgeInsets.all(10.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  child: ListBody(
+                    children: [
+                      Text(
+                        '${subjAndGrade[index][0]} - ${subjAndGrade[index][1]}',
+                        textAlign: TextAlign.left,
+                        textDirection: TextDirection.ltr,
+                        style: style.copyWith(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    if (semestergpa[index][0] != "Backlogs" &&
+                        semestergpa[index][0] != "Total GPA") {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SemesterData(index),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              );
+            },
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          ButtonTheme(
+            child: Center(
+              child: Ink(
+                decoration: const ShapeDecoration(
+                  color: Color(0xff3949ab),
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  color: Color(0xfff5f7ff),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+        ],
       ),
     );
   }
